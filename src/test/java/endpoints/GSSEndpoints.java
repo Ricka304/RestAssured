8a76fe7f2f0a65d9c26cf02e2cf3a526e217dfe4
ricka.reddy <ricka.reddy@ril.com>
2025-09-08 19:47:20 +0530

    test commit

--- src/test/java/endpoints/GSSEndpoints.java
+++ src/test/java/endpoints/GSSEndpoints.java
@@ -4,5 +4,6 @@ public class GSSEndpoints {
     public static final String FETCH_SCHEMES = "/gssEnroll/fetchSchemes/GSS";
     public static final String EXTENSION_PATH_PARAM="/ext/enroll/api";
     public static final String PLATFORM_PATH_PARAM="/api/service/application";
+    public static final String PLATFORM_PATH_PARAM2="/api/service/application";
 
 }
